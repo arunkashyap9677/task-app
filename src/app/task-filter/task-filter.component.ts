5f08501a5e9d17f20e6ea66818b8945aca219780
Arun Kashyap <arunkashyap9677@gmail.com>
2024-06-24 21:54:56 +0530

    initial commit

---
+++ src/app/task-filter/task-filter.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-filter',
+  templateUrl: './task-filter.component.html',
+  styleUrls: ['./task-filter.component.css'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class TaskFilterComponent {
+  dropdownOpen = false;
+  selectedFilter: string | null = null;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  selectFilter(filter: string) {
+    if (this.selectedFilter === filter) {
+      this.selectedFilter = null;
+    } else {
+      this.selectedFilter = filter;
+    }
+  }
+
+  clearFilter() {
+    this.selectedFilter = null;
+  }
+}
---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../task.service';
+import { TaskFormComponent } from '../task-form/task-form.component';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css'],
+  imports: [TaskFormComponent],
+  standalone: true,
+})
+export class TaskListComponent implements OnInit {
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.taskService.getTasks().subscribe((data: any[]) => {
+      this.tasks = data;
+    });
+    console.log('logging ngOnInit TaskListComponent', this.isModalVisible);
+  }
+
+  tasks: any[] = [];
+  isModalVisible: boolean = false;
+
+  openModal() {
+    console.log('logging isVisible on open', this.isModalVisible);
+    this.isModalVisible = true;
+    console.log('logging isVisible on open', this.isModalVisible);
+  }
+
+  onModalClosed() {
+    this.isModalVisible = false;
+  }
+
+  deleteTask(taskId: number) {
+    this.taskService.deleteTask(taskId).subscribe(() => {
+      this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    });
+  }
+}
